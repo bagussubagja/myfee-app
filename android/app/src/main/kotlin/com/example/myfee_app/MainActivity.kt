16e893bc4c3e2a83e5118e7feab756ab50cddcca
bagussubagja <bagussubagja17@gmail.com>
2022-03-17 00:54:25 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/myfee_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myfee_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color greenMain = const Color(0xff63D673);
+Color greenMint = const Color(0xff3AD29F);
+Color yellowColor = const Color(0xffC2E673);
+Color blackMain = const Color(0xff444444);
+Color blackText = const Color(0xff888888);
+Color greyColor = const Color(0xffAAAAAA);
